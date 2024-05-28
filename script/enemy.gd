a7a9b5830043d2c687e76410f59fe6ba42cb6318
ieeemma <emma.freeman@protonmail.com>
2024-05-28 18:35:04 +0100

    feat(enemy): attacks

--- script/enemy.gd
+++ script/enemy.gd
@@ -2,9 +2,18 @@ extends CharacterBody2D
 class_name Enemy
 
 @export var target: Node
-@export var SPEED: float = 400
+@export var speed: float = 400
+
+@export var attack_range: float = 50
 
 func _physics_process(_delta: float):
-	var dir = (target.global_position - global_position).normalized()
-	velocity = dir * SPEED
+	var v = target.global_position - global_position
+	var dist = v.length()
+	var dir = v.normalized()
+	velocity = dir * speed
 	move_and_slide()
+
+	if dist < attack_range:
+		var health = target.get_node("Health")
+		if health:
+			health.damage(1)
