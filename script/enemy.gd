92f46d5e51fd4481ff402a1b48571c43b5cfda2f
ieeemma <emma.freeman@protonmail.com>
2024-05-28 17:54:04 +0100

    feat(enemy): random spawning

--- script/enemy.gd
+++ script/enemy.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 class_name Enemy
 
+@export var target: Node
 @export var SPEED: float = 400
 
 func _physics_process(_delta: float):
-	var dir = (%Player.global_position - global_position).normalized()
+	var dir = (target.global_position - global_position).normalized()
 	velocity = dir * SPEED
 	move_and_slide()
